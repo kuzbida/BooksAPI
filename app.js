59960f396cbc43fed077e11db6322b888302a7b0
Volodymyr K <vkuzbida@n-ix.com>
2016-10-05 19:14:14 +0300

    add fs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
+    fs = require('file-system'),
 
     //services
     auth    = require('./services/auth'),
@@ -34,6 +35,21 @@ app.post('/api/token', auth.token);
 //check token before secure endpoints
 app.use(auth.checkToken);
 
+app.post('/api/file-upload', function(req, res) {
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+};
 app.use('/api/books', bookRouter);
 app.use('/api/users', userRouter);
 
